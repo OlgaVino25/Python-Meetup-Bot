e748d864b600991f17e0dfbf73279a0f08f4fa0d
Timon <74593747+T1moooon@users.noreply.github.com>
2025-11-27 23:13:09 +0300

    add talkinline

--- app_core/admin.py
+++ app_core/admin.py
@@ -9,11 +9,30 @@ class UserAdmin(admin.ModelAdmin):
     list_editable = ["role",]
 
 
+class TalkInline(admin.TabularInline):
+    model = Talk
+    extra = 1
+    fields = ("title", "speaker", "start_time", "end_time")
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "speaker":
+            kwargs["queryset"] = User.objects.filter(role="speaker")
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ["title", "start_date", "end_date"]
+    inlines = [TalkInline]
 
 
 @admin.register(Talk)
 class TalkAdmin(admin.ModelAdmin):
     list_display = ["title", "speaker", "start_time", "event"]
+    list_filter = ("event", "speaker")
+    search_fields = ("title", "speaker__first_name", "speaker__last_name")
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "speaker":
+            kwargs["queryset"] = User.objects.filter(role="speaker")
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
