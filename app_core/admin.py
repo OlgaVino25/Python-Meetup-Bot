2069ed9bd86128db9c7d93011d8f65de572beecc
Timon <74593747+T1moooon@users.noreply.github.com>
2025-11-27 21:32:48 +0300

    clean admin

--- app_core/admin.py
+++ app_core/admin.py
@@ -4,8 +4,8 @@ from .models import *
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["first_name", "telegram_id", "role", "company"]
-    list_filter = ["role", "is_networking_active"]
+    list_display = ["first_name", "telegram_id", "role"]
+    list_filter = ["role",]
     list_editable = ["role",]
 
 
@@ -17,28 +17,3 @@ class EventAdmin(admin.ModelAdmin):
 @admin.register(Talk)
 class TalkAdmin(admin.ModelAdmin):
     list_display = ["title", "speaker", "start_time", "event"]
-
-
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ["from_user", "talk", "is_answered", "created_at"]
-
-
-@admin.register(Donation)
-class DonationAdmin(admin.ModelAdmin):
-    list_display = ["from_user", "amount", "event", "created_at"]
-
-
-@admin.register(NetworkingMatch)
-class NetworkingMatchAdmin(admin.ModelAdmin):
-    list_display = ["user1", "user2", "status", "created_at"]
-
-
-@admin.register(Broadcast)
-class BroadcastAdmin(admin.ModelAdmin):
-    list_display = ["event", "sent_by", "sent_at"]
-
-
-@admin.register(UserEvent)
-class UserEventAdmin(admin.ModelAdmin):
-    list_display = ["user", "event", "registered_at"]
